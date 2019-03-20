ed35572fe624b2c1ae3e70a20aea66f49b66c458
JamieB133 <2127147b@student.gla.ac.uk>
2019-03-20 22:48:55 +0000

    added graphs and tables

--- PlotFuzzySets.m
+++ PlotFuzzySets.m
@@ -16,13 +16,13 @@
 HeadingController = readfis('HeadingsToTurnCmd');
 MotorController = readfis('TurnCommand');
 
-HeadingInputs = {'Heading Angle', 'Reference Angle to Waypoint'};
+HeadingInputs = {'Reference Angle to Waypoint', 'Heading Angle'};
 HeadingOutput = 'Heading Command';
 MotorInputs = {'Heading Command', 'Radius from Waypoint', 'Wall Angle', 'Distance from Wall', 'Distance from Wall (filtered)'};
 MotorOutputs = {'Motor Gain', 'Right Motor Gain', 'Filter Cutoff Frequency'};
 graphTitle = ' (Membership Functions)';
 
-fileType = 'svg';
+fileType = 'eps';
 
 %inputs to heading controller
 for i = 1:2
@@ -33,7 +33,8 @@ for i = 1:2
 
     ioNum = string(i);
     fileName = strcat('./report/figures/HeadingControlInput', ioNum);
-    saveas(figure(i), fileName, fileType);
+    saveas(figure(i), fileName, fileType); %vector graphics for report
+    saveas(figure(i), fileName, 'png'); %for preview
 end;
 
 %output from heading controller
@@ -41,7 +42,9 @@ figure(3);
 plotmf(HeadingController, 'output', 1);
 headingOutputTitle = strcat(HeadingOutput, graphTitle);
 title(headingOutputTitle);
-saveas(figure(3), './report/figures/HeadingControlOutput1', fileType);
+saveas(figure(3), './report/figures/HeadingControlOutput1', fileType); %vector graphics for report
+saveas(figure(3), './report/figures/HeadingControlOutput1', 'png'); %for preview
+
 
 %inputs to motor controller
 for i = 1:5
@@ -52,7 +55,9 @@ for i = 1:5
 
     ioNum = string(i);
     fileName = strcat('./report/figures/MotorControlInput', ioNum);
-    saveas(figure(i+3), fileName, fileType);
+    saveas(figure(i+3), fileName, fileType); %vector graphics for report
+    saveas(figure(i+3), fileName, 'png'); %for preview
+
 
 end;
 
@@ -65,7 +70,8 @@ for i = 1:3
 
     ioNum = string(i);
     fileName = strcat('./report/figures/MotorControlOutput', ioNum);
-    saveas(figure(i+9), fileName, fileType);
+    saveas(figure(i+9), fileName, fileType); %vector graphics for report
+    saveas(figure(i+9), fileName, 'png'); %for preview
 end;
 
  
